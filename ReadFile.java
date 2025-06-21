73ec4c71750fac506e189edf28d419c5bc9cd064
HaihengLim <haihenglim738@gmail.com>
2025-06-21 14:19:43 +0700

    Java 21.06.2025

--- ReadFile.java
+++ ReadFile.java
@@ -2,7 +2,7 @@ import java.io.*;
 
 public class ReadFile {
     public static void main(String[] args) throws Exception{
-        FileInputStream f = new FileInputStream("d:\\Programming\\text\\text.txt");
+        FileInputStream f = new FileInputStream("d:\\Programming\\text\\text.txt"); //Your own directory file
         int c; String data = "";
         while(true){
             c = f.read();
